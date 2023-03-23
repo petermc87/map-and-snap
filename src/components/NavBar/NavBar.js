600ebdc7e77516c40897bbf986b278e6bb67e60e
petermc87 <peter.mcgibney@gmail.com>
2023-03-23 11:10:02 -0400

    Completed styling and responsiveness for Navbar

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,26 +1,34 @@
 import { Link, useNavigate } from 'react-router-dom'
 import styles from './NavBar.module.scss'
+import { useState } from 'react'
+
 
 export default function NavBar (props) {
+  const [openHamburger, setOpenHamburger] = useState(false)
+  const [hamburgerInitializer, setHamburgerInitializer] = useState(false)
+  const navigate = useNavigate()
 
-const navigate = useNavigate()
+
   return (
-    <nav className={styles.navContainer}>
-
-      {/* Navigation to a few different links using the
-      useNavigatehook */}
-
-      {/* Add in an onClick for each once the about, login
-      and services have been added */}
-      <div className={styles.navLink}>Logo</div>
-      <div className={styles.rightNavContainer}>
-        <div className={styles.navLink}>Services</div>
-        <div className={styles.navLink}>About</div>
-        <div className={styles.navLink}>Login</div>
-      </div>
-      {/* <Link to='/orders'>Order History</Link>
-        &nbsp; | &nbsp;
-      <Link to='/orders/new'>New Order</Link> */}
-    </nav>
-  )
-}
+    <>
+      <nav className={styles.navContainer}>
+        <div className={styles.navLink} id={styles.navLogo}>Logo</div>
+        <div className={styles.rightNavContainer}>
+          <div className={styles.navLink}>Services</div>
+          <div className={styles.navLink}>About</div>
+          <div className={styles.navLink}>Login</div>
+        </div>
+          {/* <Link to='/orders'>Order History</Link>
+            &nbsp; | &nbsp;
+          <Link to='/orders/new'>New Order</Link> */}
+        {!openHamburger
+          ? <div className={styles.hamburgerMenu}>
+              <div className={styles.lineOneHm} />
+              <div className={styles.lineTwoHm} />
+              <div className={styles.lineThreeHm} />
+            </div>
+          : ''}
+      </nav>
+    </>
+    )
+  }
