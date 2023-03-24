9a4f80a15fd47246313c60ee6477a66fd217acd0
petermc87 <peter.mcgibney@gmail.com>
2023-03-24 14:02:58 -0400

    Adding intro section with text and hero image

--- src/components/IntroSection/IntroSection.js
+++ src/components/IntroSection/IntroSection.js
@@ -2,6 +2,12 @@ import styles from './IntroSection.module.scss'
 
 export default function IntroSection () {
   return (
-    <div className={styles.introContainer}>IntroSection</div>
+    <div className={styles.introContainer}>
+      <div className={styles.introText}>
+        <h1>CONVENIENT, EFFICIENT & TIMELY SERVICE</h1>
+        <p>At Drone Map & Snap, we pride ourselves in good customer service. Whatever your drone needs, we have it.</p>
+      </div>
+      <div className={styles.button}>Sign Up</div>
+    </div>
   )
 }
